36d0c1c34adb8524dfc1563763e50979617bf533
abiodunfakolujo <amf28@njit.edu>
2019-03-13 21:37:25 -0400

    Add: Creation of .csv file

--- public/index.php
+++ public/index.php
@@ -6,4 +6,39 @@
  * Time: 16:41
  */
 
-echo 'Hello World';
+main::start();
+class main {
+
+    static public function start() {
+        $records = csv::getRecords();
+        $table = html::generateTable($records);
+        system::printPage($table);
+    }
+}
+
+class csv {
+
+    static public function getRecords() {
+
+        $records = 'test lolok';
+        return $records;
+    }
+}
+
+class html {
+
+    static public function generateTable($records) {
+
+        $table = $records;
+
+        return $table;
+    }
+}
+
+class system {
+
+    static public function printPage($page) {
+
+        echo $page;
+    }
+}
